502e4a4116ae137fc9af236dbab431fd698dbba8
Quinton Ashley <qashto@gmail.com>
2025-04-24 17:18:16 -0500

    1.4.0

--- sketch.js
+++ sketch.js
@@ -1,18 +1,30 @@
-let ball;
+let q = await Q5.WebGPU();
 
-function setup() {
-	new Canvas(500, 500);
-	displayMode(CENTERED);
+new Canvas();
+world.gravity.y = 10;
 
-	ball = new Sprite();
-	ball.diameter = 50;
-}
+let ball = new Sprite();
+ball.x = -200;
+ball.y = -200;
+ball.diameter = 50;
 
-function draw() {
+let groundA = new Sprite();
+groundA.x = -120;
+groundA.width = 200;
+groundA.rotation = 20;
+groundA.collider = STATIC;
+
+let groundB = new Sprite();
+groundB.x = 120;
+groundB.width = 200;
+groundB.rotation = -20;
+groundB.collider = STATIC;
+
+q.update = () => {
 	background('skyblue');
 
-	if (mouse.presses()) {
-		ball.speed = 10;
-		ball.moveTo(mouse);
-	}
-}
+	textAlign(CENTER);
+	text('click to jump!', 0, -100);
+
+	if (mouse.presses()) ball.vel.y = -5;
+};
